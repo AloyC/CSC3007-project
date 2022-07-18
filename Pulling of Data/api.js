b83b212017d363c8b72a871e2b05f67fad5fadaa
AloyC <70746955+AloyC@users.noreply.github.com>
2022-07-18 22:17:45 +0800

    Pulling of API data
    Co-Authored-By: Angela Lui <70738756+Angela-Lui@users.noreply.github.com>
    Co-Authored-By: Khloe <70754448+khloelxh@users.noreply.github.com>

---
+++ Pulling of Data/api.js
@@ -0,0 +1,35 @@
+async function fetchAPI(month,date,time) {
+    let response = await fetch(`https://api.data.gov.sg/v1/transport/carpark-availability?date_time=2021-${month}-${date}T${time}%3A00%3A00`)
+
+    // Check if able to retrieve data
+    if (response.status === 200) {
+      let data = await response.text() // Wait for data to be retrieve and store in date
+      //console.log(data)
+      return data
+    } else console.log(response.statusText) // Failure in retrieving data due to response error
+  }
+
+
+async function printdata(month,date,time){
+    let item = await fetchAPI(month,date,time)
+    const obj = JSON.parse(item)
+    const carparkdatadict = obj.items[0].carpark_data
+    var size = Object.keys(carparkdatadict).length;
+    //console.log(carparkdatadict)
+
+    var totallots1 = 0
+    var totalavalots = 0
+
+    for (let i=0; i<size; i++){
+         totallots1 += parseInt(carparkdatadict[i].carpark_info[0].total_lots)
+         totalavalots += parseInt(carparkdatadict[i].carpark_info[0].lots_available)
+         //console.log(carparkdatadict[i].carpark_info[0].total_lots)
+    }
+    console.log("total lots: " + totallots1 + " " + "ava lots: " +totalavalots)
+}
+
+let month = "06"
+let date = "30"
+let time = "09"
+
+printdata(month,date,time)
